ecbae33a85aa7a7043a530ddc1b33898d6ef7947
anac2828 <anac2828@yahoo.com>
2023-10-09 14:55:38 -0700

    created account and customer reducers and store

--- src/store.js
+++ src/store.js
@@ -1,30 +1,112 @@
-import { createStore } from 'redux';
+import { combineReducers, createStore } from 'redux';
 
-const initialState = { balance: 0, loan: 0, loandPurpose: '' };
+const initialStateAccount = { balance: 0, loan: 0, loanPurpose: '' };
+
+const initialStateCustomer = {
+  fullName: '',
+  nationalID: '',
+  createdAt: '',
+};
 
 // reudcer cannot have side effects
-function reducer(state = initialState, action) {
+function accountReducer(state = initialStateAccount, action) {
   switch (action.type) {
     case 'account/deposit':
       return { ...state, balance: state.balance + action.payload };
     case 'account/withdraw':
       return { ...state, balance: state.balance - action.payload };
-    case 'account/requestLoad':
+    case 'account/requestLoan':
       if (state.loan > 0) return state;
-      return { ...state, loan: action.payload };
+      return {
+        ...state,
+        loan: action.payload.amount,
+        loanPurpose: action.payload.purpose,
+        balance: state.balance + action.payload.amount,
+      };
     case 'account/payLoan':
       return {
         ...state,
         loan: 0,
-        loandPurpose: '',
-        balance: state.balance - action.payload,
+        loanPurpose: '',
+        balance: state.balance - state.loan,
       };
     default:
       return state;
   }
 }
 
-const store = createStore(reducer);
+function customerReducer(state = initialStateCustomer, action) {
+  switch (action.type) {
+    case 'customer/createCustomer':
+      return {
+        ...state,
+        fullName: action.payload.fullName,
+        nationalID: action.payload.nationalID,
+        createdAt: action.payload.createdAt,
+      };
 
-store.dispatch({ type: 'account/deposit', payload: 500 });
-console.log('test');
+    case 'customer/updateName':
+      return {
+        ...state,
+        fullName: action.payload.fullName,
+      };
+    default:
+      return state;
+  }
+}
+
+// Combine reducers into one
+const rootReducer = combineReducers({
+  account: accountReducer,
+  customer: customerReducer,
+});
+const store = createStore(rootReducer);
+
+// store.dispatch({ type: 'account/deposit', payload: 500 });
+// console.log(store.getState());
+// store.dispatch({ type: 'account/withdraw', payload: 200 });
+// console.log(store.getState());
+// store.dispatch({
+//   type: 'account/requestLoan',
+//   payload: { amount: 1000, purpose: 'Buy a car' },
+// });
+
+// console.log(store.getState());
+// store.dispatch({ type: 'account/payLoan' });
+// console.log(store.getState());
+
+// ACTION CREATORS
+function deposit(amount) {
+  return { type: 'account/deposit', payload: amount };
+}
+function withdraw(amount) {
+  return { type: 'account/withdraw', payload: amount };
+}
+function requestLoan(amount, purpose) {
+  return {
+    type: 'account/requestLoan',
+    payload: { amount: amount, purpose: purpose },
+  };
+}
+function payLoan() {
+  return { type: 'account/payLoan' };
+}
+
+store.dispatch(deposit(500));
+store.dispatch(withdraw(200));
+store.dispatch(requestLoan(1000, 'Buy a car'));
+store.dispatch(payLoan());
+
+function createCustomer(fullName, nationalID) {
+  return {
+    type: 'customer/createCustomer',
+    payload: { fullName, nationalID, createdAt: new Date().toISOString() },
+  };
+}
+
+function updateName(fullName) {
+  return { type: 'customer/updateName', payload: fullName };
+}
+
+store.dispatch(createCustomer('Ana Cervantes', 1234));
+console.log(store.getState());
